17466c413a7f9bfc8186fc4d5c8f0d848294ac0b
Eden <eden.alemayehut@aastustudent.edu.et>
2025-12-04 22:39:48 +0300

    Time: 23 ms (15.82%) | Memory: 19.1 MB (10.22%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,23 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i]
+        leftS = 0
+        rightS = 0
+        for i in range(len(nums)):
+            if i == 0:
+                leftS = 0
+                rightS = total - nums[i]
+            elif i == len(nums) - 1:
+                rightS = 0
+                leftS = total - nums[len(nums)-1]
+                print(leftS)
+            else:
+                leftS += nums[i-1]
+                rightS -= nums[i]
+                print(rightS)
+            if rightS == leftS:
+                return i
+        return -1
+        
