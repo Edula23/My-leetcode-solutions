20059be332f90be87b05040c2eb1d299934c174a
Eden <eden.alemayehut@aastustudent.edu.et>
2026-02-04 11:19:41 +0300

    Time: 7 ms (72.77%) | Memory: 22.3 MB (49.70%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -10,40 +10,7 @@ class Solution:
             else:
                 result[-1][1] = max(result[-1][1], interval[1])
         return result
-        # for i in range(1, len(intervals)):
-        #     if intervals[i][0] <= intervals[i-1][1]:
-        #         intervals[i] = intervals[i-1][0], max(intervals[i-1][1], intervals[i][1])
-        #         intervals.pop(i-1)
-        # return intervals
-        # for i in range(1, len(intervals)):
-        #      if intervals[i-1][1] >= intervals[i][0]:
-        #          intervals[i-1] = [intervals[i-1][0] ,max(intervals[i-1][1], intervals[i][1])]
-        #          intervals[i] = [intervals[i-1][0] ,max(intervals[i-1][1], intervals[i][1])]
-        #          if i>=2 and intervals[i-2][1] >= intervals[i-1][0]:
-        #             intervals[i-1] = [intervals[i-2][0] ,max(intervals[i-2][1], intervals[i-1][1])]
-                
-        # for i in range(len(intervals)-1):
-        #     if intervals[i] == intervals[i+1]:
-        #         intervals.pop(i)
-        # return intervals
-
-
-
-        # #         if i == len(intervals) -1:
-
         
-        # # if intervals[0][1] < intervals[1][0]:
-        # #     res.append(intervals[0])
-        # # for i in range(1, len(intervals)):
-        # #     if intervals[i-1][1] >= intervals[i][0]:
-        # #         intervals[i] = [intervals[i-1][0] ,max(intervals[i-1][1], intervals[i][1])]
-        # #         # if intervals[i-1] not in res:
-        # #         #     res.append(intervals[i-1])
-        # #         if intervals[i] not in res:
-        # #             res.append(intervals[i]) 
-        # #     else:
-        # #         res.append(intervals[i])
-        # # return res
 
 
             
