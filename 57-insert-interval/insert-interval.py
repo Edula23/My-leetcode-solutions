920e4c0a639f8efa750eb85ceae9654a9bc905ee
Eden <eden.alemayehut@aastustudent.edu.et>
2026-02-04 11:23:28 +0300

    Time: 7 ms (6.57%) | Memory: 21.4 MB (26.51%) - LeetSync

---
+++ 57-insert-interval/insert-interval.py
@@ -0,0 +1,14 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort()
+        if intervals == []:
+            return []
+        result = []
+        for interval in intervals:
+            if result == [] or result[-1][1] < interval[0]:
+                result.append(interval)
+            else:
+                result[-1][1] = max(result[-1][1], interval[1])
+        return result
+        
