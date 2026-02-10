012621fd4f4ce82e41fc5b8d79226494255a5f75
Eden <eden.alemayehut@aastustudent.edu.et>
2026-02-10 14:37:07 +0300

    Time: 9 ms (79.36%) | Memory: 19.7 MB (16.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,12 @@
 from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        di = Counter(s)
+        dis = Counter(s)
+        dit = Counter(t)
         if len(s) != len(t):
             return False
         for ch in t:
-            if ch not in di or t.count(ch) != di[ch]  :
+            # if ch not in di or t.count(ch) != di[ch]  :
+            if dit[ch] != dis[ch]:
                 return False
         return True
