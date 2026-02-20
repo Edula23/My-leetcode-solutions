e346e85cc0c41b42629925fc467a1157502579ba
Eden <eden.alemayehut@aastustudent.edu.et>
2026-02-20 09:22:27 +0300

    Time: 186 ms (11.65%) | Memory: 53.8 MB (9.35%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -5,13 +5,13 @@ class Solution:
         minn = points[0][0]
         maxx  = points[0][1]
         for i in range(1, len(points)):
-            if points[i][0] > minn and points[i][0] < maxx:
-                minn = points[i][0]
             if points[i][1] < maxx and points[i][1] > minn:
                 maxx = points[i][1]
-            if points[i][0] > maxx:
+            if points[i][0] > minn and points[i][0] < maxx:
+                minn = points[i][0]
+            elif points[i][0] > maxx:
                 minn =  points[i][0]
                 maxx = points[i][1]
-                arrows += 1
+                arrows += 1            
         return arrows
 
