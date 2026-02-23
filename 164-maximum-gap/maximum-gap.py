44741ed7928825ac470d06765869461dd6932dcb
Eden <eden.alemayehut@aastustudent.edu.et>
2026-02-23 08:23:54 +0300

    Time: 139 ms (82.62%) | Memory: 32.1 MB (47.18%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 0
+        nums.sort()
+        maxx = 0
+        for i in range(len(nums)-1):
+            if nums[i+1] - nums[i] > maxx:
+                maxx = nums[i+1] - nums[i]
+        return(maxx)
+
+        
