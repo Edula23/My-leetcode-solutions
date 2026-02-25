22aaa6d3e79fc450aafd48ffd966b28a2e0db8c1
Eden <eden.alemayehut@aastustudent.edu.et>
2026-02-25 16:54:53 +0300

    Time: 70 ms (49.60%) | Memory: 29.3 MB (45.51%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -9,17 +9,4 @@ class Solution:
             summ += nums[i+k]
             maxSum = max(maxSum, summ)
         return maxSum/k
-        # left = 0
-        # right = left + k
-        # maxAv = 0
-        # if len(nums) <= 1:
-        #     return nums[0]/k
-        # while right <= len(nums):
-        #     summ = 0
-        #     for i in range(left, right):
-        #         summ += nums[i]
-        #     average = summ/k
-        #     maxAv = max(maxAv, average)
-        #     left += 1
-        #     right += 1
-        # return maxAv
+       
