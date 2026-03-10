df2bebfedc93efaeb0a3b932a9e04fb3dbf740eb
Eden <eden.alemayehut@aastustudent.edu.et>
2026-03-10 12:40:41 +0300

    Time: 115 ms (29.92%) | Memory: 20.5 MB (68.22%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        ans = []
+        for char in s:
+            if char == "*" and len(ans) >= 1:
+                ans.pop()
+            else:
+                ans.append(char)
+        return "".join(ans)
