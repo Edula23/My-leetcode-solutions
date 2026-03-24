a40427d76bcb5c5da8318baf528504df37c9dff2
Eden <eden.alemayehut@aastustudent.edu.et>
2026-03-24 20:33:03 +0300

    Time: 4 ms (42.13%) | Memory: 19.3 MB (50.52%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int: 
+        deleted = []
+        c = 1 + k - 1
+        for i in range(1, n+1):
+            deleted.append(i)
+        def helper(deleted, c):
+            if len(deleted) == 1:
+                return deleted[0]
+            if c >= len(deleted):
+                while c >= len(deleted):
+                    c = c - len(deleted)
+            deleted.pop(c)
+            return helper(deleted, c+k-1)
+        res = helper(deleted, k-1)
+        return res 
